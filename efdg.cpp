58c0c8ff2f05820b0dc6e155c675efb05516e780
hgfejcwoefGleb <114211284+hgfejcwoefGleb@users.noreply.github.com>
2024-03-24 11:57:32 +0300

    Add files via upload

---
+++ efdg.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <stack>
+#include <string>
+
+bool isRightBracket(char open, char close) {
+    return (open == '(' && close == ')')  (open == '[' && close == ']')  (open == '{' && close == '}');
+}
+
+std::string checkBrackets(const std::string& sequence) {
+    std::stack<char> s;
+
+    for (char bracket : sequence) {
+        if (bracket == '('  bracket == '['  bracket == '{') {
+            s.push(bracket);
+        }
+        else {
+            if (s.empty() || !isRightBracket(s.top(), bracket)) {
+                return "no";
+            }
+            s.pop();
+        }
+    }
+
+    return s.empty() ? "yes" : "no";
+}
+
+int main() {
+    std::string sequence;
+    std::cin >> sequence;
+
+    std::cout << checkBrackets(sequence) << std::endl;
+
+    return 0;
+}
